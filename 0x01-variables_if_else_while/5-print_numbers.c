4b706e2c56409733d313a9fc5915fc017eca94bf
Bekcy <bereketz2007@gmail.com>
2022-07-01 08:51:01 -0700

    prints all single digit numbers of base 10 starting from 0, followed by a new line

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -8,14 +8,10 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+int n;
+for (n = 0; n < 10; n++)
 {
-if (ch != 'q' && ch != 'e')
-{
-putchar(ch);
+printf("%d\n", n);
 }
-}
-putchar(10);
 return (0);
 }
