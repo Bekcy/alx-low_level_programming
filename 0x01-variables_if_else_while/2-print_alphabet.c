f1bc4598a0d15dedf61635bf720564e0df12651c
Bekcy <bereketz2007@gmail.com>
2022-07-01 07:08:22 -0700

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**                                        
-*main -> assign random number to the variable n each time executed 
+/**             
+*main -> assign random number to the variable n each time executed
 *and prints based on a condition
-*Return: always 0                               
+*Return: always 0
 */
 int main(void)
 {
@@ -12,8 +12,7 @@ int ch;
 for (ch = 97; ch <= 122; ch++)
 {
 putchar(ch);
-} 
+}
 putchar (10);
-
 return (0);
 }
