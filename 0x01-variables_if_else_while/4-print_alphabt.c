b7d46f1c57ce8d13c53da9eccbaf22f42ae0c6cc
Bekcy <bereketz2007@gmail.com>
2022-07-01 08:10:42 -0700

    prints the alphabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,21 +1,21 @@
-#include <stdlib.h>                              
-#include <time.h>                               
-#include <stdio.h>      
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
-*main -> assign random number to the variable n each time executed
+*main ->assign random number to the variable n each time executed
 *and prints based on a condition
-*Return: always 0                    
+*Return: always 0
 */
-int main(void)                                      
-{                                                         
-char ch;                                   
-for (ch = 'a'; ch <= 'z'; ch++)                            
-{                                    
-if (ch != 'q' && ch !='e')       
-{                                                              
-putchar(ch);                                                      
-}                                                                 
-}                     
-putchar (10);                                                     
-return (0);                                               
+int main(void)
+{
+char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+if (ch != 'q' && ch != 'e')
+{
+putchar(ch);
+}
+}
+putchar(10);
+return (0);
 }
