27321de25de9393feb1776c78e78934f525953a2
Bekcy <bereketz2007@gmail.com>
2022-07-01 07:09:57 -0700

    prints the al
    phabet in lowercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**             
+/**
 *main -> assign random number to the variable n each time executed
 *and prints based on a condition
 *Return: always 0
