eed3904ed66c0254bf6d191e59f48131831b6698
Bekcy <bereketz2007@gmail.com>
2022-07-01 05:50:39 -0700

    print the last digit of the number stored in the variable n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,9 +2,9 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main -> assign random number to the variable n each time executed and prints
-*based on a condition
-*Return : always 0
+* main -> assign random number to the variable n each time executed and prints
+* based on a condition
+* Return: always 0
 */
 int main(void)
 {
