6cf4f087c26649fc51bef7178d07706ed5c551ba
Bekcy <bereketz2007@gmail.com>
2022-07-01 04:59:54 -0700

    assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-*assign random number to the variable n 
-*each time excuted based on a given condition and return 0 
-*/
+/* assign random number to the variable */
 int main(void)
 {
 int n;
