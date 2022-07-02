2d69e5bb3a3db0a707ac51e7d706520f6de5add8
Bekcy <bereketz2007@gmail.com>
2022-07-01 20:53:56 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -8,11 +8,16 @@
 */
 int main(void)
 {
-char ch;
-for (ch = 'z'; ch >= 'a'; ch--)
+int ch;
+for (ch = 48; ch >= 57; ch--)
 {
 putchar(ch);
+if(ch !=57)
+{
+putchar(44);
+putchar(32);
 }
 putchar(10);
 return (0);
+}
 }
