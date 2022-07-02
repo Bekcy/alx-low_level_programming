cc2ca6f54a6c3db5718672e733fab2f41c4ccc95
Bekcy <bereketz2007@gmail.com>
2022-07-01 19:45:40 -0700

    prints the lowercase alphabet in reverse, followed by a new line

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 /**
 *main ->assign a random number to the variable n each time executed
-*and prints number in the variable n 
+*and prints number in the variable n
 *Return: always 0
 */
 int main(void)
 {
 char ch;
-for (ch ='z'; ch >= 'a'; ch--)
-{ 
+for (ch = 'z'; ch >= 'a'; ch--)
+{
 putchar(ch);
 }
 putchar(10);
