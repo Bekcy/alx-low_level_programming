76f45f62f5da80d4b463ed878004dc8efb94b0b8
Bekcy <bereketz2007@gmail.com>
2022-07-01 20:18:26 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,13 +4,13 @@
 /**
 *main ->assign a random number to the variable n each time executed
 *and prints the number stored in the variable n
-*Returns: always 0
+*Return: always 0
 */
 int main(void)
 {
 char ch;
 int n;
-for (n = 48; n <= 57; ch++)
+for (n = 48; n <= 57; n++)
 {
 putchar(n);
 }
