75f6e2c45d4b7122a8a0cbe5156cb29dc895fd04
Bekcy <bereketz2007@gmail.com>
2022-07-01 21:38:51 -0700

    prints all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-#include <time.h>                                                                                                                  
-#include <stdio.h>                                                                                                            
-/**                                                                                                             
-*main ->assign a random number to the variable n each time executed                                                               
-*and prints the number stored in the variable n                                                                                    
-*Return: always 0                                                                                                                  
+#include <time.h>
+#include <stdio.h>
+/**
+*main ->asiign a random number to the variable n each time executed
+*and prints the number stored in n
+*Return: always 0
 */
 int main(void)
 {
