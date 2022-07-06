24d3810965e2d262eec178174f06b88b5c316004
Bekcy <bereketz2007@gmail.com>
2022-07-06 09:48:22 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,11 +1,12 @@
 #include "main.h"
 /**
 *main->test function for positive or negative
-*Return: 0
+*Return:0
 */
 int main(void)
 {
-int i = 0;
+int i;
+i = 0;
 positive_or_negative(i);
 return (0);
 }
