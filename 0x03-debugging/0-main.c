e1b2797c9910eb0b547c5867ee94260927fe71c8
Bekcy <bereketz2007@gmail.com>
2022-07-07 00:06:37 -0700

    testing postitive_or_negative()

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,19 @@
-#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+void positive_or_negative(int i)
+{
+int n;                                                     
+srand(time(0)); 
+n = i;
+if (n > 0)
+printf("%d is positive\n", n;
+if (n == 0)
+printf("%d is zero\n", n);                 
+if (n < 0)                                         
+printf("%d is negative\n", n);             
+return (0);                                       
+}
 /**
 *main->test function for positive or negative
 *Return:0
@@ -8,4 +23,5 @@ int main(void)
 int i;
 i = 0;
 positive_or_negative(i);
+return (0);
 }
