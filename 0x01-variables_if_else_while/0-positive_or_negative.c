5b6ab89b2ffe355fc80400bec058ade76d1db783
Bekcy <bereketz2007@gmail.com>
2022-07-06 23:48:39 -0700

    task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,19 @@
 * main -> assign random number to the variable n each time executed and prints
 * based on a condition
 * Return: always 0
-*/
+*/int main(void)                                                   
+{                                                                
+int n;                                                           
+srand(time(0));                                                  
+n = rand() - RAND_MAX / 2;                                       
+if (n > 0)                                                       
+printf("%d is positive\n", n);                                   
+if (n == 0)                                                      
+printf("%d is zero\n", n);                                      
+if (n < 0)                                                       
+printf("%d is negative\n", n);
+return (0);
+}  
 int main(void)
 {
 int n;
@@ -20,7 +32,7 @@ printf("%d is negative\n", n);
 return (0);
 }
 /**
-*main->test functio
+*main->test function
 *Return:0
 */
 int main(void)
