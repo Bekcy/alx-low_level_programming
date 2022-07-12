a97e5c153816709e375fc76e338a9ca66e7e2f52
Bekcy <bereketz2007@gmail.com>
2022-07-12 03:05:34 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+*puts2->puts2 function
+*@str:str param
+*/
+void puts2(char *str)
+{
+int len;
+len = strlen(str);
+for (i = 0; i < len; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
