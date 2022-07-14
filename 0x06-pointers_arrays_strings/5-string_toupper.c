66bbd3baceadd307a416c0dca3c26bc95cf59103
Bekcy <bereketz2007@gmail.com>
2022-07-14 12:27:10 -0700

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+*string_toupper->converting any string to uppercase
+*@x:string parameter
+*Return:string
+*/
+char *string_toupper(char *)
+{
+int i = 0;
+A = 65, a = 97;
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+x[i] = x[i] - 32;
+i++;
+}
+return (x);
+}
