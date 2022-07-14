bf74f18f0d7d7fbfb04c9f84db3a683a729444d2
Bekcy <bereketz2007@gmail.com>
2022-07-14 11:16:06 -0700

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+*reverse_array->reversing an array
+*@a:array a
+*@n:an element of an array
+*/
+void reverse_array(int *a, int n)
+{
+int x, y;
+for (x = 0; x < (n / 2); x++)
+{
+y = a[x];
+a[x] = a[n - x - 1];
+a[n - x - 1];
+}
+}
